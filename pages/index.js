f8880c75b0b84dc5daba20a1553b077678863770
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-06 17:45:10 -0800

    fix text loading before image

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Meta from "../partials/seo-meta";
 import styles from "../styles/Index_Page.module.css";
 import { API_URL, fromImageToUrl } from "../utils/urls";
 
-export default function Home({ product }) {
+export default function Home({ product, imageLoaded = false }) {
   return (
     <div>
       <Meta
@@ -20,46 +20,48 @@ export default function Home({ product }) {
         image={fromImageToUrl(product.image)}
       />
       <main className={styles.main}>
-        <div className={styles.main_text}>
-          <h1 className={styles.glow}>Christian Anagnostou</h1>
-          <h3>Capturing life through a lens</h3>
-          <Link href="/about">
-            <a className={styles.bouncy_borders}>about</a>
-          </Link>
-          <Link href="/albums">
-            <a className={styles.bouncy_borders} style={{ animationDelay: "0.1s" }}>
-              albums
-            </a>
-          </Link>
-          <div className={styles.social_links_container}>
-            <Link href="/contact">
-              <a className={styles.social_link}>
-                <AiOutlineMail />
+        {imageLoaded && (
+          <div className={styles.main_text}>
+            <h1 className={styles.glow}>Christian Anagnostou</h1>
+            <h3>Capturing life through a lens</h3>
+            <Link href="/about">
+              <a className={styles.bouncy_borders}>about</a>
+            </Link>
+            <Link href="/albums">
+              <a className={styles.bouncy_borders} style={{ animationDelay: "0.1s" }}>
+                albums
               </a>
             </Link>
-            <a
-              href="https://www.instagram.com/christian.anagnostou/"
-              target="_blank"
-              className={styles.social_link}
-            >
-              <AiOutlineInstagram />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/ChristianAnagnostou/"
-              target="_blank"
-              className={styles.social_link}
-            >
-              <AiOutlineLinkedin />
-            </a>
-            <a
-              href="https://github.com/ChristianAnagnostou"
-              target="_blank"
-              className={styles.social_link}
-            >
-              <AiOutlineGithub />
-            </a>
+            <div className={styles.social_links_container}>
+              <Link href="/contact">
+                <a className={styles.social_link}>
+                  <AiOutlineMail />
+                </a>
+              </Link>
+              <a
+                href="https://www.instagram.com/christian.anagnostou/"
+                target="_blank"
+                className={styles.social_link}
+              >
+                <AiOutlineInstagram />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ChristianAnagnostou/"
+                target="_blank"
+                className={styles.social_link}
+              >
+                <AiOutlineLinkedin />
+              </a>
+              <a
+                href="https://github.com/ChristianAnagnostou"
+                target="_blank"
+                className={styles.social_link}
+              >
+                <AiOutlineGithub />
+              </a>
+            </div>
           </div>
-        </div>
+        )}
         <div className={styles.image_container}>
           <img className={styles.image} src={fromImageToUrl(product.image)} alt={product.name} />
         </div>
@@ -72,5 +74,5 @@ export async function getStaticProps() {
   const product_res = await fetch(`${API_URL}/products/?name=De-Escalator`);
   const product = await product_res.json();
 
-  return { props: { product: product[0] } };
+  return { props: { product: product[0], imageLoaded: true } };
 }
