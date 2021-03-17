6fe7a557a392cbe2614d429e44ffb8019d88ca4b
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-17 08:22:44 -0700

    rebuild header / footer UI

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
+import styles from "../styles/Footer.module.css";
+
 const Footer = () => {
   return (
-    <footer style={{ paddingBottom: "2rem" }}>
-      <p>Made with 🤍 by Christian Anagnostou</p>
-      <p>© 2021 - A WILD CHRISTIAN</p>
-      <p>ALL RIGHTS RESERVED</p>
+    <footer className={styles.footer}>
+      {/* <p>Made with 🤍 by Christian Anagnostou</p> */}
+      <p>© 2021 - A WILD CHRISTIAN. ALL RIGHTS RESERVED</p>
     </footer>
   );
 };
--- components/Header.js
+++ components/Header.js
@@ -1,43 +1,27 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 import AuthContext from "../context/AuthContext";
 import styles from "../styles/Header.module.css";
-import NavLinks from "./NavLinks";
+import HeaderIcon from "../SVGs/HeaderIcon";
+import ButtonLink from "./ButtonLink";
 
 const Header = () => {
   const { user } = useContext(AuthContext);
-
-  const router = useRouter();
-
-  const [mobileNavActive, setMobileNavActive] = useState(false);
-
-  const toggleMobileNav = (e) => {
-    setMobileNavActive(!mobileNavActive);
-  };
-
-  useEffect(() => {
-    setMobileNavActive(false);
-  }, [router]);
 
   return (
     <header className={styles.header}>
       <div className={styles.title}>
         <Link href="/">
           <a>
-            <h1>Christian Anagnostou</h1>
+            <HeaderIcon />
           </a>
         </Link>
       </div>
-      <div className={styles.bars} onClick={toggleMobileNav}>
-        <div className={`${styles.line} ${mobileNavActive && styles.active_line1}`} />
-        <div className={`${styles.line} ${mobileNavActive && styles.active_line2}`} />
-        <div className={`${styles.line} ${mobileNavActive && styles.active_line3}`} />
-      </div>
-      <nav className={`${styles.nav} ${mobileNavActive && styles.showNav}`}>
-        <div className={styles.shadow} onClick={() => setMobileNavActive(false)} />
-        <NavLinks />
+
+      <nav className={styles.nav}>
+        <ButtonLink text="CONTACT" color="orange" route="/contact" />
+
         <div className={styles.auth}>
           {user ? (
             <Link href="/account">
@@ -46,11 +30,7 @@ const Header = () => {
               </a>
             </Link>
           ) : (
-            <Link href="/login">
-              <a className={`${styles.login} ${router.route === "/login" && styles.highlight}`}>
-                Log in
-              </a>
-            </Link>
+            <ButtonLink text="LOGIN" color="blue" route="/login" />
           )}
         </div>
       </nav>
