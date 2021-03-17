4b9744533189db72ecc3085be90d98a84f6c6d2c
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-17 08:22:00 -0700

    add: useKeyPressHook

--- hooks/index.js
+++ hooks/index.js
@@ -1,4 +1,5 @@
 import { useIsInView } from "./useIsInView";
 import { useOrders } from "./useOrders";
+import { useKeyPress } from "./useKeyPress";
 
-export { useIsInView, useOrders };
+export { useIsInView, useOrders, useKeyPress };
