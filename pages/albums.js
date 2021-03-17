4c47a58d08f0936c2bffd2b6a2306c7f31d316e2
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-17 08:26:04 -0700

    rebuild album page UI

--- pages/albums.js
+++ pages/albums.js
@@ -1,30 +1,103 @@
-import styles from "../styles/Gallery.module.css";
-import { API_URL } from "../utils/urls";
-import { InViewImage } from "../components/InViewImage";
-import Meta from "../partials/seo-meta";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-const albums = ({ albums }) => {
+import styles from "../styles/Albums.module.css";
+import SocialIcons from "../components/SocialIcons";
+import { API_URL, fromImageToUrl } from "../utils/urls";
+import Meta from "../partials/seo-meta";
+import { LeftArrow, RightArrow } from "../SVGs/Arrows";
+import Circle from "../SVGs/Circle";
+import { useKeyPress } from "../hooks";
+
+const albums = ({ albums, loaded = false }) => {
+  // Reverse albums so that they are layered properly with position absolute stacking
+  const [albumOrder, setAlbumOrder] = useState([...albums]);
+  const [currentAlbumIndex, setCurrentAlbumIndex] = useState(1);
+
+  // Rotates the albumOrder and tracks currentAlbumIndex
+  const shiftAlbumView = (direction) => {
+    switch (direction) {
+      case "right":
+        const first = albumOrder.shift();
+        setAlbumOrder([...albumOrder, first]);
+
+        setCurrentAlbumIndex(currentAlbumIndex < albums.length ? currentAlbumIndex + 1 : 1);
+        break;
+      case "left":
+        const last = albumOrder.pop();
+        setAlbumOrder([last, ...albumOrder]);
+
+        setCurrentAlbumIndex(currentAlbumIndex === 1 ? albums.length : currentAlbumIndex - 1);
+        break;
+    }
+  };
+
+  // Use arrows to navigate albums
+  const rightKeyPress = useKeyPress("ArrowRight");
+  const leftKeyPress = useKeyPress("ArrowLeft");
+
+  useEffect(() => {
+    rightKeyPress && shiftAlbumView("right");
+    leftKeyPress && shiftAlbumView("left");
+  }, [rightKeyPress, leftKeyPress]);
+
   return (
     <>
       <Meta
         title="Albums - A Wild Christian"
-        desc="All albums on A Wild Christian. View and purchase prints of Christian Anagnostou's favorite photographs."
-        canonical="https://awildchristian.com/albums"
+        desc="Browse all albums. Select an album to view more and purchase prints of Christian Anagnostou's favorite photographs."
+        canonical="https://awildchristian.com/testAlbums"
       />
-      <div className={styles.product_container}>
-        {albums.map((album) => (
-          <div className={styles.album} key={album.name}>
-            <InViewImage
-              image={album.products[0].image}
-              href={`/albums/${album.slug}`}
-              imageStyles={styles.product_img}
-              threshold={0.2}
+
+      <main className={styles.main_container}>
+        <aside className={styles.aside}>
+          <div className={styles.slider}>
+            <div
+              className={styles.slider_num}
+              style={{ top: `${(currentAlbumIndex - 1) * 30 + 5}px` }}
             >
-              <p className={styles.album_name}>{album.name}</p>
-            </InViewImage>
+              {currentAlbumIndex}
+            </div>
+            <div className={styles.slider_bar}>
+              <div
+                className={styles.slider_thumb}
+                style={{ top: `${(currentAlbumIndex - 1) * 30}px` }}
+              />
+            </div>
           </div>
-        ))}
-      </div>
+          <SocialIcons direction="column" />
+        </aside>
+
+        {loaded && (
+          <section className={styles.album_container}>
+            <div className={styles.album_controller}>
+              <div onClick={() => shiftAlbumView("left")}>
+                <LeftArrow color="#fff" />
+              </div>
+
+              <div className={styles.album_fan} style={{ transform: `rotate(${3 * 5}deg)` }}>
+                {albumOrder.slice(albumOrder.length - 4).map((album, i) => (
+                  <Link key={album.slug} href={`/albums/${album.slug}`}>
+                    <img
+                      src={fromImageToUrl(album.products[0].image)}
+                      style={{ transform: `rotate(${-5 * i}deg)` }}
+                    />
+                  </Link>
+                ))}
+              </div>
+
+              <div onClick={() => shiftAlbumView("right")}>
+                <RightArrow color="#fff" />
+              </div>
+            </div>
+            <div className={styles.album_text}>
+              <h1>{albumOrder[albumOrder.length - 1].name}</h1>
+              <p>{albumOrder[albumOrder.length - 1].description}</p>
+            </div>
+          </section>
+        )}
+        <Circle styles={styles.circle} />
+      </main>
     </>
   );
 };
@@ -33,7 +106,7 @@ export async function getStaticProps() {
   const album_res = await fetch(`${API_URL}/albums/`);
   const albums = await album_res.json();
 
-  return { props: { albums } };
+  return { props: { albums, loaded: true } };
 }
 
 export default albums;
