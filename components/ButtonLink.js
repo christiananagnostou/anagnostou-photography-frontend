e5db506778291e8ba6031208e68f0daab570b274
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-23 11:13:41 -0700

    add: ButtonLink css module

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -1,28 +1,10 @@
 import Link from "next/link";
-
-const buttonStyles = {
-  border: "none",
-  padding: "0.7rem 1.3rem",
-  marginRight: "10px",
-  color: "white",
-  fontSize: "1rem",
-  fontWeight: 100,
-  cursor: "pointer",
-};
+import styles from "../styles/ButtonLink.module.css";
 
 const ButtonLink = ({ text, color, route }) => {
-  const getColorHex = (color) => {
-    switch (color) {
-      case "orange":
-        return "#F9A620";
-      case "blue":
-        return "#3A4555";
-    }
-  };
-
   return (
     <Link href={route ? route : "#"}>
-      <a style={{ ...buttonStyles, background: getColorHex(color) }}>{text}</a>
+      <a className={`${styles.button} ${styles[color]}`}>{text}</a>
     </Link>
   );
 };
