869d65e94189d08c2757ed63a04e82cdd7416251
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-03-24 10:02:32 -0700

    add SmallArrows

--- components/SVGs/Arrows.js
+++ components/SVGs/Arrows.js
@@ -33,3 +33,43 @@ export const LeftArrow = ({ color = "#fff" }) => {
     </svg>
   );
 };
+
+export const SmallArrowDown = ({ color = "#fff" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15.886"
+      height="9.055"
+      viewBox="0 0 15.886 9.055"
+    >
+      <path
+        id="Path_5"
+        data-name="Path 5"
+        d="M7.944,106.2a1.109,1.109,0,0,1-.786-.326L.327,99.04A1.112,1.112,0,0,1,1.9,97.466l6.044,6.044,6.044-6.044A1.112,1.112,0,0,1,15.56,99.04L8.73,105.87A1.109,1.109,0,0,1,7.944,106.2Z"
+        transform="translate(-0.001 -97.141)"
+        fill={color}
+      />
+    </svg>
+  );
+};
+
+export const SmallArrowUp = ({ color = "#fff" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15.886"
+      height="9.055"
+      viewBox="0 0 15.886 9.055"
+    >
+      <g id="arrow-down-sign-to-navigate" transform="translate(84.999 -100.141)">
+        <path
+          id="Path_5"
+          data-name="Path 5"
+          d="M7.944,106.2a1.109,1.109,0,0,1-.786-.326L.327,99.04A1.112,1.112,0,0,1,1.9,97.466l6.044,6.044,6.044-6.044A1.112,1.112,0,0,1,15.56,99.04L8.73,105.87A1.109,1.109,0,0,1,7.944,106.2Z"
+          transform="translate(-69.112 206.336) rotate(180)"
+          fill={color}
+        />
+      </g>
+    </svg>
+  );
+};
