2d187cf3c461942d7b416863e2c341ee5e62b1af
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-05-10 20:28:50 -0700

    give icon links aria-label

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -7,13 +7,31 @@ import InstagramLogo from "./SVGs/InstagramLogo";
 const SocialIcons = ({ direction = "row" }) => {
   return (
     <div className={`${styles.social_icons} ${direction === "row" ? styles.row : styles.column}`}>
-      <a href="https://github.com/ChristianAnagnostou" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/ChristianAnagnostou"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Github"
+        aria-required="true"
+      >
         <GitHubLogo />
       </a>
-      <a href="https://www.linkedin.com/in/ChristianAnagnostou" target="_blank" rel="noreferrer">
+      <a
+        href="https://www.linkedin.com/in/ChristianAnagnostou"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="LinkedIn"
+        aria-required="true"
+      >
         <LinkedInLogo />
       </a>
-      <a href="https://www.instagram.com/christian.anagnostou/" target="_blank" rel="noreferrer">
+      <a
+        href="https://www.instagram.com/christian.anagnostou/"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Instagram"
+        aria-required="true"
+      >
         <InstagramLogo />
       </a>
     </div>
